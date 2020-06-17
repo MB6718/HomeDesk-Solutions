1ef9700408a53a69775590010dc0302f16c029ee
Gift-Dar <64541060+Gift-Dar@users.noreply.github.com>
2020-06-17 16:05:16 +0500

    Аутентификация пользователя login/logout

--- src/blueprints/auth.py
+++ src/blueprints/auth.py
@@ -1,24 +1,45 @@
 from flask import (
-	Blueprint,
-	request,
-	session
+    Blueprint,
+    request,
+    session,
 )
-
-from werkzeug.security import (
-	generate_password_hash,
-	check_password_hash
-)
+from werkzeug.security import check_password_hash
 
 from database import db
 
+
 bp = Blueprint('auth', __name__)
 
-@bp.route('/login', methods=["POST"])
-def login():
-	""" Обработка аутентификации пользователя """
-	return 'login - OK', 200
 
-@bp.route('/logout', methods=["POST"])
+@bp.route('/login', methods=['POST'])
+def login():
+    request_json = request.json
+    email = request_json.get('email')
+    password = request_json.get('password')
+
+    if not email or not password:
+        return '', 400
+
+    with db.connection as con:
+        cur = con.execute(
+            'SELECT * '
+            'FROM account '
+            'WHERE email = ?',
+            (email,),
+        )
+        user = cur.fetchone()
+
+    if user is None:
+        return '', 404
+
+    if not check_password_hash(user['password'], password):
+        return '', 403
+
+    session['account_id'] = user['id']
+    return 'login - OK', 200
+
+
+@bp.route('/logout', methods=['POST'])
 def logout():
-	""" Обработка выхода(деаутентификации) пользователя """
-	return 'logout - OK', 200
+    session.pop('account_id', None)
+    return 'logout - OK', 200
