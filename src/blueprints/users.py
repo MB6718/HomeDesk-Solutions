d310f3633b5d889b119f2a25de13bc4e552215df
Xandarel <aobordug@gmail.com>
2020-06-17 14:16:27 +0500

    (feature) Реализована регистрация пользователя

--- src/blueprints/users.py
+++ src/blueprints/users.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 from flask import (
 	Blueprint,
 	request,
@@ -17,8 +19,27 @@ from src.services.users import UsersService
 
 bp = Blueprint('users', __name__)
 
+
 @bp.route('', methods=["POST"])
 def users():
 	""" Обработка регистрации нового пользователя """
-	#""" проверка коммита """
-	return 'users POST - OK', 200
+	print('here')
+	request_json = request.json
+
+	email = request_json.get('email')
+	first_name = request_json.get('first_name')
+	last_name = request_json.get('last_name')
+	password = request_json.get('password')
+
+	password_hash = generate_password_hash(password)
+	with db.connection as con:
+		try:
+			con.execute("""
+				INSERT INTO Account (first_name, last_name, email, password)
+				VALUES (?, ?, ?, ?)
+				""",(first_name, last_name, email, password_hash)
+				)
+			con.commit()
+		except sqlite3.IntegrityError:
+			return 'Данный пользователь уже существует', 409
+	return request_json, 200
