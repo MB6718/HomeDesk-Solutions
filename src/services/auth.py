57df1532fa34630720c3db5f170d8c7ee6cb8c9b
Gift-Dar <64541060+Gift-Dar@users.noreply.github.com>
2020-06-24 12:45:50 +0500

    Аутентификация пользователя вынесена в отдельный сервис. Исправлен код ошибки 404  на 403

---
+++ src/services/auth.py
@@ -0,0 +1,14 @@
+class AuthService:
+    def __init__(self, connection):
+		self.connection = connection
+ 
+    def login_user(self, email):
+        cur = self.connection.cursor()
+        cur.execute(
+            'SELECT * '
+            'FROM accounts '
+            'WHERE email = ?',
+            (email,),
+        )
+        user = cur.fetchone()
+        return user
