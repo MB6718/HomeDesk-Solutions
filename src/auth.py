f8cc5ed067e4f575032f726981708f106bfb7ace
MB6718 <mb6718@mail.ru>
2020-06-24 15:13:59 +0500

    fix(auth.py) Исправлен баг с отступом во wrapper

--- src/auth.py
+++ src/auth.py
@@ -18,23 +18,23 @@ def must_be_owner(of):
     def decorator(view_func):
         @wraps(view_func)
         def wrapper(*args, **kwargs):
-                name = {
-                    'category': 'categories',
-                    'transaction': 'transactions',
-                }[of]
-                with db.connection as con:
-                    cur = con.execute(f"""
-                        SELECT f.account_id
-                        FROM {name} AS f
-                        WHERE f.id = ?
-                        """,
-                        (kwargs[f'{of}_id'],)
-                    )
-                    result = cur.fetchone()
-                if not result:
-                    return '', 404
-                if result['account_id'] != kwargs['account_id']:
-                    return '', 403
+            name = {
+                'category': 'categories',
+                'transaction': 'transactions',
+            }[of]
+            with db.connection as con:
+                cur = con.execute(f"""
+                    SELECT f.account_id
+                    FROM {name} AS f
+                    WHERE f.id = ?
+                    """,
+                    (kwargs[f'{of}_id'],)
+                )
+                result = cur.fetchone()
+            if not result:
+                return '', 404
+            if result['account_id'] != kwargs['account_id']:
+                return '', 403
             return view_func(*args, **kwargs)
         return wrapper
     return decorator
