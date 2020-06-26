f14b749cdbf2f6c9323311f8fdadc3949a5b8ed2
MB6718 <mb6718@mail.ru>
2020-06-26 15:10:09 +0500

    refactor(categories) Рефакторинг кода сервиса категорий
    - Перенесены в код функции из сервиса transactions
    - Изменена логика обработки ошибок (exceptions), отвязана от сущности и вынесена во вне

--- src/exceptions.py
+++ src/exceptions.py
@@ -3,3 +3,17 @@ class ServiceError(Exception):
 
     def __init__(self, *args):
         super().__init__(self.service, *args)
+
+# class CategoriesServiceError(ServiceError):
+    # service = 'categories'
+
+class ConflictError(ServiceError):
+    def __init__(self, category):
+        ServiceError.__init__(self)
+        self.category = category
+
+class CategoryDoesNotExistError(ServiceError):
+    pass
+
+class PermissionError(ServiceError):
+    pass
